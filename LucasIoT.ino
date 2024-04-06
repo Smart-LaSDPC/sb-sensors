4376e7958855dd8c30447e19ca2f3b31c9dfd282
edsonmagno <80956435+edsonmagno@users.noreply.github.com>
2024-04-06 11:21:55 -0300

    Update LucasIoT.ino

--- LucasIoT.ino
+++ LucasIoT.ino
@@ -1,1 +1,15 @@
-j
+//Libs do espnow e wifi
+#include <esp_now.h>
+#include <Wifi.h>
+
+//Canal usado para conexão
+#define CHANNEL 1
+
+//PInos que iremos ler (digitalRead) e enviar para os Slaves
+//É importante que o código fonte dos Slaves tenha este mesmo array com os mesmos //gpios
+//na mesma ordem
+
+uintB gpios[] = {23,2};
+
+// No setup
+
